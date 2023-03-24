38054efc6abfa5d260b0f0cb8cc018e8308df442
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-03-24 06:48:44 +0500

    added sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
+import Navbar from "components/Navbar";
+import PageLines from "components/PageLines";
+import Portal from "components/Portal";
+import StripeDivider from "components/StripeDivider";
+import Hero from "sections/Hero";
+import Presale from "sections/Presale";
+import Products from "sections/Products";
+import Whitepaper from "sections/Whitepaper";
 
 function Index() {
   return (
-    <div>
-      <h1>Index</h1>
-    </div>
+    <Box pb={"100px"}>
+      <Portal>
+        <PageLines />
+      </Portal>
+
+      <Navbar />
+      <Hero />
+      <StripeDivider />
+      <Presale />
+      <Whitepaper />
+      <Products />
+
+      <Box bg="black.primary"></Box>
+    </Box>
   );
 }
 
